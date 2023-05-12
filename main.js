c4aba0fd15898b2eea9eb4d9b853c8d5b7dbbf94
Hassan <hasan.f2002@gmail.com>
2023-05-12 11:41:45 +0300

    js Dropdown

---
+++ main.js
@@ -0,0 +1,22 @@
+//icon drop down rotation
+let dropbtn1 = document.getElementById("drop-btn1");
+let dropicon1 = document.getElementById("drop-icon1");
+dropbtn1.addEventListener("click",()=>{
+    dropicon1.classList.toggle("rotate");
+})
+
+let dropbtn2 = document.getElementById("drop-btn2");
+let dropicon2 = document.getElementById("drop-icon2");
+dropbtn2.addEventListener("click",()=>{
+    dropicon2.classList.toggle("rotate");
+})
+let dropbtn3 = document.getElementById("drop-btn3");
+let dropicon3 = document.getElementById("drop-icon3");
+dropbtn3.addEventListener("click",()=>{
+    dropicon3.classList.toggle("rotate");
+})
+let dropbtn4 = document.getElementById("drop-btn4");
+let dropicon4 = document.getElementById("drop-icon4");
+dropbtn4.addEventListener("click",()=>{
+    dropicon4.classList.toggle("rotate");
+})
